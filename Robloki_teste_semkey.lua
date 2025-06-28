c7d272e44653285a0c0834d2da8ded30ebc938c9
Dr4gonX <oiranzinho00@gmail.com>
2025-06-28 17:53:10 -0300

    Robloki_teste_semkey.lua

---
+++ Robloki_teste_semkey.lua
@@ -0,0 +1,4 @@
+-- Executa diretamente o script pelo link
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dr4gonScripts/Syteam-keys/refs/heads/main/D4gonHub.lua"))()
+end)
